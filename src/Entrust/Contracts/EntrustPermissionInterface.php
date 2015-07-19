61bd2741007a787a1a344131b72f08199a0785ed
Mohammad Reza <parsidev@users.noreply.github.com>
2015-07-19 19:27:27 +0430

    Release

---
+++ src/Entrust/Contracts/EntrustPermissionInterface.php
@@ -0,0 +1,20 @@
+<?php namespace Parsidev\Entrust\Contracts;
+
+/**
+ * This file is part of Entrust,
+ * a role & permission management solution for Laravel.
+ *
+ * @license MIT
+ * @package Parsidev\Entrust
+ */
+
+interface EntrustPermissionInterface
+{
+    
+    /**
+     * Many-to-Many relations with role model.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function roles();
+}
